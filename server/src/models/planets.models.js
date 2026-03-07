ae0a91de4a19166eca4059d995c5fb8af078310b
Rishabh Shukla <shuklarishabh487@gmail.com>
2026-03-07 17:05:13 -0600

    feat: integrated mongodb database and added function to perform core database operations through controllers

--- server/src/models/planets.models.js
+++ server/src/models/planets.models.js
@@ -2,7 +2,7 @@ const path = require("path");
 const fs = require("fs");
 const { parse } = require("csv-parse");
 
-const habitablePlanets = [];
+const planets = require('./planets.mongo')
 
 function isHabitablePlanet(planet) {
   return planet['koi_disposition'] === 'CONFIRMED'
@@ -21,25 +21,49 @@ function loadPlanetsData(){
     comment: '#',
     columns: true,
   }))
-  .on('data', (data) => {
+  .on('data', async (data) => {
     if (isHabitablePlanet(data)) {
-      habitablePlanets.push(data);
+      await savePlanets(data)
     }
   })
   .on('error', (err) => {
     console.log(err);
     reject(err);
   })
-  .on('end', () => {
-    console.log(`${habitablePlanets.length} habitable planets found!`);
+  .on('end', async () => {
+    const countPlanets = (await getAllPlanets()).length
+    console.log(`${countPlanets} habitable planets found!`);
     resolve();
   });
 });
 }
 
 
+async function savePlanets(planet){
+  try{
+      await planets.updateOne({
+        keplerName: planet.kepler_name,
+      }, {
+        keplerName: planet.kepler_name,
+      }, {
+        upsert: true,
+      });
+  } catch(err){
+      console.error(`Could not save planet: ${err}`)
+  }
+  
+}
+
+
+async function getAllPlanets(){
+  return await planets.find({}, {
+    '_id': 0,
+    '__v': 0,
+  })
+}
+
 
 module.exports = {
-  planets: habitablePlanets,
-  loadPlanetsData
+  loadPlanetsData,
+  getAllPlanets,
 };
--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,9 +1,10 @@
-const {planets} = require('../../models/planets.models')
+const { getAllPlanets } = require('../../models/planets.models')
 
-function getAllPlanets(req, res){
-    return res.status(200).json(planets)
+async function httpGetAllPlanets(req, res){
+    const planets = await getAllPlanets();
+    return res.status(200).json(planets);
 }
 
 module.exports = {
-    getAllPlanets
+    httpGetAllPlanets
 }
